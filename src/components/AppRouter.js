1ac2d0feda831b4f62a229172b37688988b902a6
Luiz <luizrodrigo46@hotmail.com>
2018-05-01 18:53:13 -0300

    CreatePage added

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import HomePage from './HomePage.js';
+import CreatePage from './CreatePage.js';
 
 const AppRouter = () => (
   <Router>
@@ -9,11 +10,15 @@ const AppRouter = () => (
         <li>
           <Link to="/">Home</Link>
         </li>
+        <li>
+          <Link to="/create">Create Questions</Link>
+        </li>
       </ul>
 
       <hr />
 
       <Route exact path="/" component={HomePage} />
+      <Route exact path="/create" component={CreatePage} />
     </div>
   </Router>
 );
---
+++ src/components/CreatePage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CreatePage = () => {
+    return (
+        <div>
+            <h2>
+                Create Questions
+            </h2>
+        </div>
+    );
+}
+
+export default CreatePage
