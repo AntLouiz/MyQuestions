2d9dc556814dd783651cf037cd3a4553303a26bf
Luiz <luizrodrigo46@hotmail.com>
2018-05-15 11:57:27 -0300

    empty questionnarie added to create page

--- src/pages/CreatePage.js
+++ src/pages/CreatePage.js
@@ -1,14 +1,26 @@
 import React from 'react'
+import shortid from 'shortid'
 import Questionnarie from '../components/Questionnarie.js'
 
 const CreatePage = () => {
+    const empty_questionnarie = {
+        id: shortid.generate(),
+        title: undefined,
+        description: undefined,
+        questions: []
+    }
     return (
         <div>
             <h2>
                 Create Questions
             </h2>
             <div className="container">
-                <Questionnarie />
+                <Questionnarie
+                    id={empty_questionnarie.id}
+                    title={empty_questionnarie.title}
+                    description={empty_questionnarie.description}
+                    questions={empty_questionnarie.questions}
+                />
             </div>
         </div>
     );
