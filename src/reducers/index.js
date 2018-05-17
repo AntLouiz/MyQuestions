f125ecc670bdf591c48308bd0129be42e0d64250
Luiz <luizrodrigo46@hotmail.com>
2018-05-17 00:00:24 -0300

    reducer updated

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,22 +2,6 @@ import { combineReducers } from 'redux'
 import QuestionnarieReducer from './QuestionnarieReducer.js'
 
 
-const FetchReducer = (state, action) => {
-    switch (action.type){
-        case 'FETCH_START':
-            console.log("STATING")
-            return true
-
-        case 'FETCH_CLOSE':
-            return false
-
-        default:
-            return false
-    }
-}
-
-
 export default combineReducers({
-  questionnarie: QuestionnarieReducer,
-  is_loading: FetchReducer
+  questionnaries: QuestionnarieReducer,
 })
