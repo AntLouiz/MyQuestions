6a1c07d150b3806e47d13740717020ce8c2a1318
Luiz <luizrodrigo46@hotmail.com>
2018-05-17 00:00:04 -0300

    some changes on the ListPage

---
+++ src/pages/DetailPage.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import shortid from 'shortid'
+import Questionnarie from '../components/Questionnarie.js'
+import { connect } from 'react-redux'
+import { fetchQuestionnarieById } from '../actions'
+import { Redirect } from "react-router-dom"
+
+class DetailPage extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            questionnarie_id: props.match.params.id,
+        }
+    }
+
+    componentWillMount() {
+        this.props.fetchQuestionnarieById(this.state.questionnarie_id);
+    }
+
+    render() {
+        if(this.props.is_loading)
+            return (<div>loading...</div>)
+        else if(this.props.data){
+            console.log(this.props.data)
+            return (
+                <div>
+                    <Questionnarie
+                        id={this.props.data.id}
+                        title={this.props.data.title}
+                        description={this.props.data.description}
+                        questions={this.props.data.questions}
+                        is_saved={true}
+                    />
+                </div>
+            );
+        }
+        else
+            return <Redirect to="/list"/>
+    }
+}
+
+const mapStateToProps = (state) => ({
+    data: state.questionnaries.data,
+    is_loading: state.questionnaries.is_loading
+})
+
+const mapDispatchToProps = (dispatch) => ({
+    fetchQuestionnarieById: (questionnarie_id) => {
+        dispatch(fetchQuestionnarieById(questionnarie_id));
+    }
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(DetailPage)
--- src/pages/ListPage.js
+++ src/pages/ListPage.js
@@ -2,66 +2,50 @@ import React from 'react'
 import { connect, dispatch } from 'react-redux'
 import { fetchQuestionnaries } from '../actions'
 import Questionnarie from '../components/Questionnarie.js'
-import { startFetch } from '../actions/fetch.js'
 import { Link } from 'react-router-dom'
 
 class ListPage extends React.Component {
     constructor(props) {
         super(props);
+        console.log(props);
     }
 
     componentWillMount() {
-        this.props.startFetch();
         this.props.fetchQuestionnaries();
     }
 
     render() {
-        const Questionnaries = (props) => {
-            if(this.props.is_loading){
-                return (
-                    <span>Loading...</span>
-                );
-            }
-            else if(!this.props.questionnaries.length)
-                return (<div>Questionnaries not found</div>);
-            else{
-                return (
-                    <ul>
-                        {this.props.questionnaries.map((questionnarie) => {
-                            return  <li key={questionnarie.id}>
-                                        <Link to={`/detail/${questionnarie.id}`}>
-                                            {questionnarie.title}
-                                        </Link>
-                                    </li>
-                        })}
-                    </ul>
-                );
-            }
+        if(this.props.questionnaries.length){
+            return (<ul>
+                    {this.props.questionnaries.map((questionnarie) => {
+                        return (
+                            <li key={questionnarie.id}>
+                                <Link to={`/detail/${questionnarie.id}`}>
+                                    {questionnarie.title}
+                                </Link>
+                            </li>
+                        );
+                    })}
+                </ul>);
         }
-
-        return (
-            <div>
-                <Questionnaries questionnaries={this.props.questionnaries} />
-            </div>
-        );
+        else if(this.props.is_loading)
+            return <div>Loading...</div>
+        else
+            return <div>Not found questionnaries</div>
     }
 }
 
 const mapStateToProps = (state) => {
-    console.log(state.questionnarie)
     return {
-        questionnaries: state.questionnarie,
-        is_loading: state.is_loading
+        questionnaries: state.questionnaries.data,
+        is_loading: state.questionnaries.is_loading
     }
 }
 
 const mapDispatchToProps = (dispatch) => ({
   fetchQuestionnaries: () => {
     dispatch(fetchQuestionnaries())
-  },
-  startFetch: () => {
-    dispatch(startFetch())
-  },
+  }
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(ListPage)
