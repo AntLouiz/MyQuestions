95510f532526370d72c94aa74e72ad7fd544851d
Luiz <luizrodrigo46@hotmail.com>
2018-05-16 23:59:50 -0300

    Detail page router added

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -8,6 +8,7 @@ import {
 import HomePage from '../pages/HomePage.js';
 import CreatePage from '../pages/CreatePage.js';
 import ListPage from '../pages/ListPage.js';
+import DetailPage from '../pages/DetailPage.js';
 import NotFoundPage from '../pages/NotFoundPage.js';
 import Menu from './Menu.js';
 
@@ -20,6 +21,7 @@ const AppRouter = () => (
         <Route exact path="/" component={HomePage} />
         <Route exact path="/create" component={CreatePage} />
         <Route exact path="/list" component={ListPage} />
+        <Route path="/detail/:id" component={DetailPage} />
         <Route component={NotFoundPage} />
       </Switch>
 
