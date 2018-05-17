292edde38927be750ef11efe29c3a69a87c556a7
Luiz <luizrodrigo46@hotmail.com>
2018-05-17 18:58:43 -0300

    create page updated with a modal

--- src/pages/CreatePage.js
+++ src/pages/CreatePage.js
@@ -15,12 +15,34 @@ const CreatePage = () => {
                 Create Questions
             </h2>
             <div className="container">
-                <Questionnarie
-                    id={empty_questionnarie.id}
-                    title={empty_questionnarie.title}
-                    description={empty_questionnarie.description}
-                    questions={empty_questionnarie.questions}
-                />
+                <div class="modal is-active">
+                <div class="modal-background"></div>
+                    <div class="modal-content">
+                          <div class="modal-card">
+                            <header class="modal-card-head">
+                              <p class="modal-card-title">Create a new questionnarie</p>
+                              <button class="delete" aria-label="close"></button>
+                            </header>
+                            <section class="modal-card-body">
+                              <div>
+                                <label to="input_title">
+                                    <h2>Title:</h2>
+                                </label>
+                                <input 
+                                    className="input is-primary" 
+                                    name="input_title" 
+                                    type="text" 
+                                />
+                              </div>
+                            </section>
+                            <footer class="modal-card-foot">
+                              <button class="button is-success">Create</button>
+                              <button class="button">Cancel</button>
+                            </footer>
+                          </div>
+                    </div>
+                <button class="modal-close"></button>
+                </div>
             </div>
         </div>
     );
