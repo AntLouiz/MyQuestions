66d427ffde81e8ada6d70ff5b4eb067e6c63df48
AntLouiz <luizrodrigo46@hotmail.com>
2018-05-23 15:05:31 -0300

    logo removed

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -4,11 +4,6 @@ import { Link } from 'react-router-dom'
 const Menu = () => {
     return (
       <nav className="navbar" role="navigation" aria-label="main navigation">
-        <div className="navbar-brand">
-          <a className="navbar-item" href="https://bulma.io">
-            <img src="http://www.iconsdb.com/icons/preview/color/23D160/literature-xxl.png" />
-          </a>
-        </div>
         <div className="navbar-menu">
           <Link to="/" className="navbar-item button is-white">
             Home
