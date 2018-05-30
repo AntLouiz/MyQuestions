4f0f7f35df09f4b0d977bca9497813c26edc43ba
AntLouiz <luizrodrigo46@hotmail.com>
2018-05-30 15:29:26 -0300

    QuestionnaireCard refactored

--- src/components/questionnaire_list/QuestionnaireCard.js
+++ src/components/questionnaire_list/QuestionnaireCard.js
@@ -1,44 +1,46 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const QuestionnaireCard = (props) => {
-    return (
-        <div className="column is-12">
-            <div className="card">
-                <Choose>
-                    <When condition={props.is_active}>
-                        <Link className="" to={`/questionnaire/detail/${props._key}/${props.id}`}>
+class QuestionnaireCard extends React.Component {
+    render() {
+        return (
+            <div className="column is-12">
+                <div className="card">
+                    <Choose>
+                        <When condition={this.props.is_active}>
+                            <Link className="" to={`/questionnaire/detail/${this.props._key}/${this.props.id}`}>
+                                <header className="card-header has-background-info">
+                                    <p className="card-header-title">
+                                        {this.props.title}
+                                    </p>
+                                </header>
+                            </Link>
+                            <footer className="card-footer">
+                                <a href="/" className="card-footer-item">
+                                    Compound
+                                </a>
+                                <a href="/" className="card-footer-item">
+                                    Archive
+                                </a>
+                            </footer>
+                        </When>
+                        <Otherwise>
                             <header className="card-header has-background-info">
                                 <p className="card-header-title">
-                                    {props.title}
+                                    {this.props.title}
                                 </p>
                             </header>
-                        </Link>
-                        <footer className="card-footer">
-                            <a href="/" className="card-footer-item">
-                                Compound
-                            </a>
-                            <a href="/" className="card-footer-item">
-                                Archive
-                            </a>
-                        </footer>
-                    </When>
-                    <Otherwise>
-                        <header className="card-header has-background-info">
-                            <p className="card-header-title">
-                                {props.title}
-                            </p>
-                        </header>
-                        <footer className="card-footer">
-                            <a href="/" className="card-footer-item">
-                                Restore
-                            </a>
-                        </footer>
-                    </Otherwise>
-                </Choose>
+                            <footer className="card-footer">
+                                <a href="/" className="card-footer-item">
+                                    Restore
+                                </a>
+                            </footer>
+                        </Otherwise>
+                    </Choose>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default QuestionnaireCard;
